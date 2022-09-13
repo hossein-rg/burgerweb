1fb625f3a78efe1ea63e17c8898cee84b3072d90
hossein <hosinrg1380@gmail.com>
2022-09-14 04:29:19 +0430

    set module for bank sec5 and sec6

---
+++ source/js/bankSection5.js
@@ -0,0 +1,18 @@
+let bankSec5 = {
+    srcMain : ["image/img/sec5/sec5-f1.svg",
+               "image/img/sec5/sec5-f2.svg",
+               "image/img/sec5/sec5-f3.svg",
+               "image/img/sec5/sec5-f4.svg",
+               "image/img/sec5/sec5-f5.svg",
+               "image/img/sec5/sec5-f6.svg"],
+    srcHover:["image/img/sec5/sec5-f1hover.svg",
+              "image/img/sec5/sec5-f2hover.svg",
+              "image/img/sec5/sec5-f3hover.svg",
+              "image/img/sec5/sec5-f4hover.svg",
+              "image/img/sec5/sec5-f5hover.svg",
+              "image/img/sec5/sec5-f6hover.svg"]
+    }
+
+    export {bankSec5};
+
+  
